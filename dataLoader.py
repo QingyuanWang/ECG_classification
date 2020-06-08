bfeb270ef4c96aa9a1e7c007aaf71fbdfbfc9ac6
qingyuan <qingyuan.wang@ucdconnect.ie>
2020-06-08 21:37:12 +0800

    using argparse

--- dataLoader.py
+++ dataLoader.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class DataLoader:
-    def __init__(self, X, Y, train_val_split=[0.7, 0.15, 0.15], shuffle=True, batch_size=100):
+    def __init__(self, X, Y, train_val_split=[0.7, 0.15, 0.15], shuffle=True, batch_size=100, device='cpu'):
         if shuffle:
             p = np.random.permutation(range(len(X)))
             X, Y = X[p], Y[p]
@@ -14,8 +14,8 @@ class DataLoader:
         for i in range(len(train_val_split)):
             start_index = end_index
             end_index += int(len(X) * train_val_split[i])
-            X_tensor = torch.tensor(X[start_index:end_index], dtype=torch.float).permute([0, 2, 1])
-            Y_tensor = torch.tensor(Y[start_index:end_index], dtype=torch.long)
+            X_tensor = torch.tensor(X[start_index:end_index], dtype=torch.float, device=device).permute([0, 2, 1])
+            Y_tensor = torch.tensor(Y[start_index:end_index], dtype=torch.long, device=device)
             dataset = Data.TensorDataset(X_tensor, Y_tensor)
             self.loader_list.append(Data.DataLoader(
                 dataset=dataset,
--- main.py
+++ main.py
@@ -4,28 +4,46 @@ import torch
 import os
 from trainer import Trainer
 from dataLoader import DataLoader
-import sys
+import argparse
 
 
-torch.manual_seed(1)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoint_dir/')
+    parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('--dataset_path', type=str, default='./data.npz')
+    args = parser.parse_known_args()[0]
+    return args
 
-checkpoint_dir = "./checkpoint_dir/"
+
+args = get_args()
+torch.manual_seed(args.seed)
+device = args.device
+checkpoint_dir = args.checkpoint_dir
 if not os.path.exists(checkpoint_dir):
     os.makedirs(checkpoint_dir)
 
-data = np.load('./data.npz')
+data = np.load(args.dataset_path)
 
-model = net()
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+model = net().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-dataLoader = DataLoader(data['X'], data['Y'], train_val_split=[0.7, 0.15, 0.15], batch_size=int(sys.argv[1]))
+dataLoader = DataLoader(data['X'],
+                        data['Y'],
+                        train_val_split=[0.7, 0.15, 0.15],
+                        batch_size=args.batch_size,
+                        device=device)
 loader_train, loader_val, loader_test = dataLoader.get_loader()
 trainer = Trainer(model, optimizer)
 loss_fn = torch.nn.functional.cross_entropy
 trainer.train_with_val(loss_fn,
                        loader_train=loader_train,
                        loader_val=loader_val,
-                       epochs=int(sys.argv[2]),
+                       epochs=args.epochs,
                        save_path=checkpoint_dir + 'model.pth',
                        save_best_only=True,
                        monitor_on='acc')
